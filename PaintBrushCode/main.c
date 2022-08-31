3bdf53a6cdbf52f2c8ecc7e205328436a6acde51
Marco Antônio Almeida <marco.almeida@sou.unaerp.edu.br>
2022-08-31 08:39:55 -0300

    project initial scope

--- PaintBrushCode/main.c
+++ PaintBrushCode/main.c
@@ -1,8 +1,31 @@
+//Declaração das bibliotecas
+#include "windows.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+
+//Declaração de constantes
+#define _WIN32_WINNT 0x0600
+
+//Declaração de variáveis globais
+int j=0;
 
 int main()
 {
-    printf("Hello world!\n");
+    //-----------------------------------------------------
+    // Inicializa o console grafico
+    //---------------------------------------------
+    HWND meu_console = GetConsoleWindow();
+    HDC meu_dc = GetDC(meu_console);
+    COLORREF COR= RGB(255,255,0);   /* cor amarela */
+    printf("Algoritmo de Breseham...");
+    printf(" ");
+    //=============================================================
+
+    //SEU CODIGO GRAFICO ENTRA AQUI
+
+    //=============================================================
+    ReleaseDC(meu_console, meu_dc);
+    getch();
     return 0;
 }
